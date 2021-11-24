67a804969de8dfcb1b9b816b77929ba47fad9698
neelfirst <neelfirst@gmail.com>
2021-11-24 13:59:16 -0800

    add dfk py

---
+++ dfk.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import requests, json
+
+URL = 'https://graph2.defikingdoms.com/subgraphs/name/defikingdoms/apiv5'
+N_HEROES = 45000 # would be lovely to retrieve this value from the api
+QUERY = """query getHeroInfo($I: Int) {
+  heros(skip: $I, orderBy: numberId) {
+    id
+    owner {
+      id
+      name
+    }
+  }
+}"""
+
+def getHero():
+  return
+
+def getAllHeroes():
+  results = []
+  for i in range(0, N_HEROES, 100): # we're limited to 100 heroes at a time
+    vars = {'I': i}
+    r = requests.post(URL, json={'query': QUERY, 'variables': vars}).json()['data']['heros']
+    results.extend(r)
+  return results
+
+def levelAllHeroes():
+  return
