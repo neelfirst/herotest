56560779c436acdf813bede799f84486ed268594
neelfirst <neelfirst@gmail.com>
2021-11-25 20:02:09 +0000

    all bar charts display with legend

--- app.py
+++ app.py
@@ -3,23 +3,22 @@ import matplotlib.pyplot as plt
 from flask import Flask, request, url_for, render_template, send_file, redirect
 from dfk import *
 
+STAT_LIST = ['strength', 'endurance', 'agility', 'luck', 'dexterity', 'intelligence', 'wisdom', 'vitality']
+
 app = Flask(__name__)
 # before the app starts, index all available heroes
 # data.txt indexes the first 44089 heroes (scraped 2021-11-24)
 hero_list = json.load(open('data.txt','r'))
 df_hero = pandas.DataFrame(hero_list)
-sys.stdout.flush()
-print(df_hero['strength'])
 
 # stat: string name
 # hero_id: our query
 def getBarChart(stat, hero):
   xy = {}
-  xy['stat'] = df_hero[stat]
-  xy['count'] = df_hero[stat].value_counts()
-  df_plot = pandas.DataFrame(xy)
+  xy[stat] = df_hero[stat].value_counts().sort_index(ascending=True).keys()
+  xy['count'] = df_hero[stat].value_counts().sort_index(ascending=True).values
   buf = io.BytesIO()
-  ax = df_plot.plot.bar(rot=0)
+  ax = pandas.DataFrame(xy).plot.bar(x=stat, y='count', rot=0)
   plt.savefig(buf)
   buf.seek(0)
   return buf
@@ -27,10 +26,10 @@ def getBarChart(stat, hero):
 def generatePage(name, images):
   string = "<html><head><title>"+name+"</title></head><body>"
   for i in range(0,len(images)):
-    p = os.path.join(os.getcwd(), 'static',  name + str(i) + '.png')
+    p = os.path.join(os.getcwd(), 'static',  name + '_' + str(i) + '.png')
     with open(p,'wb') as ifile:
       ifile.write(images[i].read())
-    img = url_for('static', filename=name+str(i)+'.png')
+    img = url_for('static', filename=name+'_'+str(i)+'.png')
     string += "<img src=\""+img+"\"><br>"
   string += "</body></html>"
   return(string)
@@ -48,8 +47,9 @@ def search():
 @app.route("/<string:name>/")
 def say_hello(name):
   images = []
-  images.append(getBarChart('strength', name))
-  return generatePage('test', images)
+  for stat in STAT_LIST:
+    images.append(getBarChart(stat, name))
+  return generatePage(name, images)
 
 def main():
   # remove lag from app, level all heroes in advance
