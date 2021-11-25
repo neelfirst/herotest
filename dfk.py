9663819bb523e14f590c23afb0dd2f1f4ffaac67
neelfirst <neelfirst@gmail.com>
2021-11-24 17:47:11 -0800

    working batch query

--- dfk.py
+++ dfk.py
@@ -1,18 +1,11 @@
 #!/usr/bin/env python3
 
 import requests, json
+from time import sleep
+from querydfk import BATCH_QUERY
 
 URL = 'https://graph2.defikingdoms.com/subgraphs/name/defikingdoms/apiv5'
 N_HEROES = 45000 # would be lovely to retrieve this value from the api
-QUERY = """query getHeroInfo($I: Int) {
-  heros(skip: $I, orderBy: numberId) {
-    id
-    owner {
-      id
-      name
-    }
-  }
-}"""
 
 def getHero():
   return
@@ -21,9 +14,9 @@ def getAllHeroes():
   results = []
   for i in range(0, N_HEROES, 100): # we're limited to 100 heroes at a time
     vars = {'I': i}
-    r = requests.post(URL, json={'query': QUERY, 'variables': vars}).json()['data']['heros']
+    r = requests.post(URL, json={'query': BATCH_QUERY, 'variables': vars}).json()['data']['heros']
     results.extend(r)
-    print(i)
+    sleep(1)
   return results
 
 def levelAllHeroes():
